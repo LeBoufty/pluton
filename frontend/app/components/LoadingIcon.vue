7b3dba26ae9d5c604b7496def2724842d1f1de0d
LeBoufty <boufto@gmail.com>
2026-04-08 14:12:09 +0200

    functioning animation

--- frontend/app/components/LoadingIcon.vue
+++ frontend/app/components/LoadingIcon.vue
@@ -1,43 +1,83 @@
 <template>
-    <div>
-        <Centerer>
-            <img class="sun" src="~/assets/img/loading/sun.png" />
-        </Centerer>
-        <div class="loading_leftright">
-            <img class="loading_updown" src="~/assets/img/loading/planet.png" />
+    <Transition>
+        <div class="loading_wrapper">
+            <div class="system">
+                <div class="sun">
+                    <img src="~/assets/img/loading/sun.png" />
+                </div>
+                <div class="planet">
+                    <img src="~/assets/img/loading/planet.png" />
+                </div>
+            </div>
         </div>
-    </div>
+    </Transition>
 </template>
 
 <style>
-.sun {
-    position: absolute;
-    margin-top: 8em;
-    margin-left: 14em;
-    width: 7%;
-}
-.loading_leftright {
-    position: absolute;
-    width: 100vw;
-    height: 100vh;
-    animation: leftRight 2s alternate infinite ease-in-out;
-}
-.loading_updown {
-    position: absolute;
-    animation: upDown 2s alternate infinite ease-in-out;
-    animation-delay: 1000ms;
-    width: 3%;
+/* repeatedly hit my head with a hammer trying to do this
+until this goat saved my life (and my skull)
+https://codersblock.com/blog/making-orbit-animations-with-css-custom-properties/ */
+
+@property --angle {
+    syntax: "<angle>";
+    inherits: true;
+    initial-value: 0deg;
 }
 
-@keyframes upDown {
+@property --z {
+    syntax: "<integer>";
+    inherits: true;
+    initial-value: 0;
+}
+
+@keyframes revolve {
+    from {
+        --angle: -180deg;
+        --z: 0;
+    }
     to {
-        transform: translatey(100px);
-        opacity: if(media(y < 0): 0 else 1);
+        --angle: 180deg;
+        --z: 100;
     }
 }
-@keyframes leftRight {
-    to {
-        transform: translatex(200px);
-    }
+
+.loading_wrapper {
+    overflow: hidden;
+}
+
+.system {
+    display: grid;
+    place-items: center;
+    height: 100vh;
+    margin: 0;
+    padding-top: 17vh;
+    background-color: #141733;
+    overflow: clip;
+}
+
+.planet {
+    --x-amplitude: 100px;
+    --y-amplitude: 40px;
+    --x: calc(cos(var(--angle)) * var(--x-amplitude));
+    --y: calc(sin(var(--angle)) * var(--y-amplitude) - 50vh);
+    translate: var(--x) var(--y);
+    z-index: calc(var(--z)); /* calc() is for Safari */
+    animation: revolve 6s linear infinite;
+}
+
+img {
+    display: block;
+}
+
+.sun {
+    z-index: 50;
+}
+
+.sun img {
+    width: 100%;
+}
+
+.planet img {
+    width: 100%;
 }
 </style>
--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,19 +1,19 @@
 <template>
-  <Centerer>
-    <img src="~/assets/img/plutonfulltitle.png" id="full-logo" />
-    <p id="sub-text">anxiolytics for your email... 💤</p>
-    <a href="/accounts/list">accounts/list</a>
-    <a href="/accounts/new">accounts/new</a>
-  </Centerer>
+    <Centerer>
+        <img src="~/assets/img/plutonfulltitle.png" id="full-logo" />
+        <p id="sub-text">anxiolytics for your email... 💤</p>
+        <a href="/accounts/list">accounts/list</a>
+        <a href="/accounts/new">accounts/new</a>
+    </Centerer>
 </template>
 
 <style>
 #full-logo {
-  height: 50%;
+    height: 50%;
 }
 #sub-text {
-  margin-top: -70px;
-  margin-bottom: 50px;
-  font-style: italic;
+    margin-top: -70px;
+    margin-bottom: 50px;
+    font-style: italic;
 }
 </style>
