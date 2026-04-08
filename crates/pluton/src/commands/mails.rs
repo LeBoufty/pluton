7697da5bfe9ad889bf65de3a4b49e8014a76600d
LeBoufty <boufto@gmail.com>
2026-04-09 00:21:04 +0200

    commands for mailboxes

---
+++ crates/pluton/src/commands/mails.rs
@@ -0,0 +1,43 @@
+use std::sync::Mutex;
+
+use mail_parser::Message;
+use pluton_core::accounts::AccountID;
+use tauri::State;
+
+use crate::{
+  commands::{CommandError, CommandResult},
+  state::PlutonState,
+};
+
+#[tauri::command]
+pub fn get_mailboxes_for_id(
+  state: State<'_, Mutex<PlutonState>>,
+  account_id: &AccountID,
+) -> CommandResult<Vec<String>> {
+  let mut state = state.lock().map_err(|_| CommandError::InvalidMutexAccess)?;
+  let interfaces = &mut state.mail_interfaces;
+  let interface = interfaces
+    .get_mut(account_id)
+    .map_err(|_| CommandError::InvalidAccountID)?;
+  let mailboxes = interface
+    .get_mailboxes()
+    .map_err(|e| CommandError::PlutonMailError(e))?;
+  Ok(mailboxes.clone())
+}
+
+#[tauri::command]
+pub fn get_email_headers_for_box(
+  state: State<'_, Mutex<PlutonState>>,
+  account_id: &AccountID,
+  mailbox: &str,
+) -> CommandResult<Vec<(u64, Message<'static>)>> {
+  let mut state = state.lock().map_err(|_| CommandError::InvalidMutexAccess)?;
+  let interfaces = &mut state.mail_interfaces;
+  let interface = interfaces
+    .get_mut(account_id)
+    .map_err(|_| CommandError::InvalidAccountID)?;
+  let emails = interface
+    .get_email_headers_in_mailbox(mailbox)
+    .map_err(|e| CommandError::PlutonMailError(e))?;
+  Ok(emails.clone())
+}
--- crates/pluton/src/commands/mod.rs
+++ crates/pluton/src/commands/mod.rs
@@ -8,30 +8,33 @@ pub mod mails;
 /// serialized command errors for typescript error management
 #[derive(Debug, Error)]
 pub enum CommandError {
-  #[error("fatal error while trying to access app state")]
-  InvalidMutexAccess,
+    #[error("fatal error while trying to access app state")]
+    InvalidMutexAccess,
 
-  #[error(transparent)]
-  PlutonCoreError(#[from] PlutonCoreError),
+    #[error("the account ID that was provided is invalid")]
+    InvalidAccountID,
 
-  #[error(transparent)]
-  PlutonMailError(#[from] MailError),
+    #[error(transparent)]
+    PlutonCoreError(#[from] PlutonCoreError),
+
+    #[error(transparent)]
+    PlutonMailError(#[from] MailError),
 }
 
 impl From<CommandError> for String {
-  fn from(value: CommandError) -> Self {
-    value.to_string()
-  }
+    fn from(value: CommandError) -> Self {
+        value.to_string()
+    }
 }
 
 // Add this for Tauri compatibility
 impl serde::Serialize for CommandError {
-  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-  where
-    S: serde::Serializer,
-  {
-    serializer.serialize_str(&self.to_string())
-  }
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
 }
 
 pub type CommandResult<R> = Result<R, CommandError>;
