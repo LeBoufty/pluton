74d6dce2ab49603637c22d0aee8a3d15a0e49311
LeBoufty <boufto@gmail.com>
2026-04-08 11:52:15 +0200

    Attempt at a loading icon

---
+++ frontend/app/components/LoadingIcon.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <Centerer>
+            <img class="sun" src="~/assets/img/loading/sun.png" />
+        </Centerer>
+        <div class="loading_leftright">
+            <img class="loading_updown" src="~/assets/img/loading/planet.png" />
+        </div>
+    </div>
+</template>
+
+<style>
+.sun {
+    position: absolute;
+    margin-top: 8em;
+    margin-left: 14em;
+    width: 7%;
+}
+.loading_leftright {
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    animation: leftRight 2s alternate infinite ease-in-out;
+}
+.loading_updown {
+    position: absolute;
+    animation: upDown 2s alternate infinite ease-in-out;
+    animation-delay: 1000ms;
+    width: 3%;
+}
+
+@keyframes upDown {
+    to {
+        transform: translatey(100px);
+        opacity: if(media(y < 0): 0 else 1);
+    }
+}
+@keyframes leftRight {
+    to {
+        transform: translatex(200px);
+    }
+}
+</style>
